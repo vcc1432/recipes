d4fb9aaaf1f8c881b42ec88c825e9231f71fc837
vcc1432 <valerie.gersen@gmail.com>
2018-10-31 23:03:16 +0100

    Basic bootstrap navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import RecipesListContainer from './components/RecipesListContainer'
 import RecipeDetailsContainer from './components/RecipeDetailsContainer'
 import Home from './components/Home'
+import TopNav from './components/TopNav'
+
 
 class App extends Component {
   render() {
     return (
       <Router>
           <div>
+            <TopNav />
             <Route exact path="/" component={Home} />
             <Route exact path="/recipes" component={RecipesListContainer} />
             <Route path="/recipes/:id" component={RecipeDetailsContainer} />
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,29 +4,11 @@ import './Home.css'
 
 export default function Home() {
   return (
-  <div class='text-center wrapper'>
-    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-      <header class="masthead mb-auto">
-        <div class="inner">
-          <nav class="nav nav-masthead justify-content-center">
-          </nav>
-        </div>
-      </header>
-
-      <main role="main" class="inner cover">
-        <h1 class="cover-heading">Uber for tickets</h1>
-        <p class="lead">We want customers to offer tickets to events in Amsterdam and when you buy them, we automatically dispatch a car to pick you up to the event.</p>
-        <p class="lead">
-          <Link to="/recipes" style={{ textDecoration: 'none' }} class="btn btn-lg btn-secondary">See recipes</Link>
-        </p>
-      </main>
-
-      <footer class="mastfoot mt-auto"> 
-        <div class="inner">
-          <p>Valerie</p>
-        </div>
-      </footer>
+    <header class="masthead d-flex">
+    <div class="container text-center my-auto">
+      <h1 class="mb-1">Valerie's Recipe Collection</h1>
+      <Link to="/recipes" style={{ textDecoration: 'none' }} class="btn btn-primary btn-xl js-scroll-trigger">See all recipes</Link>
     </div>
-      
-    </div>)
+    <div class="overlay"></div>
+  </header>)
 }
